d334d0358d6f28eafd286355077d95bb23b8bed1
guangl <guangluo@outlook.com>
2021-02-19 23:51:44 +0800

    first commit

---
+++ init.zsh
@@ -0,0 +1,2 @@
+[ -f ~/.config/zsh/mapping.zsh ] && source $HOME/.config/zsh/mapping.zsh
+[ -f ~/.config/zsh/export.zsh ] && source $HOME/.config/zsh/export.zsh
---
+++ mapping.zsh
@@ -0,0 +1,11 @@
+# Terminal
+alias cls='clear'
+alias vim='nvim'
+alias ls='exa'
+
+# Config File
+alias zr='source $HOME/.zshrc'
+alias zv='vim $HOME/.zshrc'
+
+# Trash
+alias rm='trash'
