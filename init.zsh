4c6652da2712a2e59bcbbd94ee3aed9123517294
guangl <guangluo@outlook.com>
2021-02-20 00:20:03 +0800

    add new keymap & new plugin

--- init.zsh
+++ init.zsh
@@ -1,2 +1,3 @@
-[ -f ~/.config/zsh/mapping.zsh ] && source $HOME/.config/zsh/mapping.zsh
-[ -f ~/.config/zsh/export.zsh ] && source $HOME/.config/zsh/export.zsh
+[ -f $HOME/.config/zsh/mapping.zsh ] && source $HOME/.config/zsh/mapping.zsh
+[ -f $HOME/.config/zsh/export.zsh ] && source $HOME/.config/zsh/export.zsh
+[ -f $HOME/.config/zsh/function.zsh ] && source $HOME/.config/zsh/function.zsh
--- mapping.zsh
+++ mapping.zsh
@@ -9,3 +9,27 @@ alias zv='vim $HOME/.zshrc'
 
 # Trash
 alias rm='trash'
+
+# Brew
+alias bi='brew install'
+alias bs='brew search'
+alias br='brew uninstall'
+alias bci='brew install --cask'
+alias bcr='brew uninstall --cask'
+alias bsero='brew services start'
+alias bserc='brew services stop'
+
+# Git
+alias ga='git add'
+alias gi='git init'
+alias gll='git pull'
+alias gsh='git push'
+alias gcm='git commit -m'
+
+# Npm
+alias ni='npm install'
+alias nr='npm run'
+alias ngi='npm install -g'
+alias ngr='npm uninstall -g'
+alias ngl='npm list -g --depth 0'
+alias ngupdate='npm-check -gu'
